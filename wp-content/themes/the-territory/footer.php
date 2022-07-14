a5399a770125bbda1bc535aa97590452b885fc01
Pea <pea@misfist.com>
2022-07-13 22:53:52 -0400

    Add after_header and before_footer hooks

--- wp-content/themes/the-territory/footer.php
+++ wp-content/themes/the-territory/footer.php
@@ -11,19 +11,31 @@
 
 ?>
 
+	<?php do_action( 'before_footer' ); ?>
+	
 	<footer class="site-footer">
+
+		<?php
+		if ( is_active_sidebar( 'footer' ) ) :
+			?>
+			<div class="footer-widgets widget-area">
+				<?php dynamic_sidebar( 'footer' ); ?>
+			</div><!-- .footer-widgets-->
+			<?php
+		endif;
+		?>
 
 		<nav id="site-footer-navigation" class="footer-navigation navigation-menu" aria-label="<?php esc_attr_e( 'Footer Navigation', 'the-territory' ); ?>">
 			<?php
 			wp_nav_menu(
-				[
+				array(
 					'fallback_cb'    => false,
 					'theme_location' => 'footer',
 					'menu_id'        => 'footer-menu',
 					'menu_class'     => 'menu container',
 					'container'      => false,
 					'depth'          => 1,
-				]
+				)
 			);
 			?>
 		</nav><!-- #site-navigation-->
