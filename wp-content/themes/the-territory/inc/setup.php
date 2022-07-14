3ffa0dab66bdaf547c07c3ce5f58f32a71d57c55
Pea <pea@misfist.com>
2022-07-13 22:52:37 -0400

    Coding standards

--- wp-content/themes/the-territory/inc/setup.php
+++ wp-content/themes/the-territory/inc/setup.php
@@ -46,11 +46,11 @@ function the_territory_setup() {
 
 	// Register navigation menus.
 	register_nav_menus(
-		[
+		array(
 			'primary' => esc_html__( 'Primary Menu', 'the-territory' ),
 			'footer'  => esc_html__( 'Footer Menu', 'the-territory' ),
 			'mobile'  => esc_html__( 'Mobile Menu', 'the-territory' ),
-		]
+		)
 	);
 
 	/**
@@ -59,13 +59,13 @@ function the_territory_setup() {
 	 */
 	add_theme_support(
 		'html5',
-		[
+		array(
 			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
 			'caption',
-		]
+		)
 	);
 
 	// Set up the WordPress core custom background feature.
@@ -73,23 +73,23 @@ function the_territory_setup() {
 		'custom-background',
 		apply_filters(
 			'the_territory_custom_background_args',
-			[
+			array(
 				'default-color' => 'ffffff',
 				'default-image' => '',
-			]
+			)
 		)
 	);
 
 	// Custom logo support.
 	add_theme_support(
 		'custom-logo',
-		[
+		array(
 			'height'      => 55,
 			'width'       => 180,
 			'flex-height' => true,
 			'flex-width'  => true,
-			'header-text' => [ 'site-title', 'site-description' ],
-		]
+			'header-text' => array( 'site-title', 'site-description' ),
+		)
 	);
 
 	// Add theme support for selective refresh for widgets.
